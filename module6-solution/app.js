aa1cd4c68d24b1fb7a5279f4db1d0c1f4701776d
Megan Rutch <mrutch1@jh.edu>
2026-03-02 20:29:15 -0500

    colors working

--- module6-solution/app.js
+++ module6-solution/app.js
@@ -12,6 +12,7 @@
             if ($scope.textEntered == undefined || $scope.textEntered == "") {
                 // need to check if empty before can split
                 $scope.message = "Please enter data first";
+                $scope.textClass = {color: "red", border: "2px solid red"};
             }
             else {
                 var splitWords = $scope.textEntered.split(',');
@@ -21,14 +22,17 @@
                 if (splitWords.length == 0) {
                     // should display Please enter data first
                     $scope.message = "Please enter data first";
+                     $scope.textClass = {color: "red", border: "2px solid red"};
                 }
                 else if (splitWords.length <= 3) {
                     // should display Enjoy!
                     $scope.message = "Enjoy!";
+                    $scope.textClass = {color: "green", border: "2px solid green"};
                 }
                 else {
                     // should display Too much!
                     $scope.message = "Too much!";
+                    $scope.textClass = {color: "green", border: "2px solid green"};
                 }
             }
         }
